0bf0e80bb1ea5176cb4d2cea02c12b2571f8fe0d
Alasdair Macindoe <agm24@st-andrews.ac.uk>
2017-10-17 22:26:00 +0100

    Script changes

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,7 +2,9 @@
 #Originally from: https://github.com/gap-packages/datastructures/blob/master/scripts/build_gap.sh
 #GNU GPL 2: https://github.com/gap-packages/datastructures/blob/master/COPYRIGHT.md
 set -ex
-
+pwd
+cd $GAPROOT
+cd ..
 GAP="$GAPROOT/bin/gap.sh --quitonbreak -q"
 
 $GAP tst/testall.g
