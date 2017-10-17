b22dec5257a3c9863f937eb527bf7b55242e7a2b
Alasdair Macindoe <agm24@st-andrews.ac.uk>
2017-10-17 22:45:42 +0100

    Added Ward to installation

--- scripts/build_gap.sh
+++ scripts/build_gap.sh
@@ -6,15 +6,15 @@ set -ex
 # build GAP in a subdirectory
 git clone --depth=2 https://github.com/gap-system/gap.git $GAPROOT
 cd $GAPROOT
+cd hpcgap
+git clone https://github.com/gap-system/ward
+cd ward
+./build.sh
+cd $GAPROOT
 ./autogen.sh
 ./configure --enable-hpcgap
 make -j4 V=1
 make bootstrap-pkg-minimal
-
-if [[ $ABI == 32 ]]
-then
-    CONFIGFLAGS="CFLAGS=-m32 LDFLAGS=-m32 LOPTS=-m32 CXXFLAGS=-m32"
-fi
 
 # build some packages...
 cd pkg
